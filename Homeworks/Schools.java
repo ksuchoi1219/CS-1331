d2aa980071e21461026362ef5f1823b0dcfe6984
Kwang Su Choi <ksuchoi1219@gmail.com>
2016-07-29 22:20:50 -0400

    Delete Schools.java

--- Homeworks/Schools.java
+++
@@ -1,6 +0,0 @@
-public class Schools {
-    public static void main(String[] args) {
-        String gt = "Georgia Institute of Technology";
-        System.out.printf("%s > %s%n", gt, uga);
-    }
-}
